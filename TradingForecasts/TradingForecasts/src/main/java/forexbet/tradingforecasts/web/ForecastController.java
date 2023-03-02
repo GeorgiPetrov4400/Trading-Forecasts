7abccfec102a48d2bc4e58683d347e34137858ac
Georgi Petrov <gap4400@gmail.com>
2023-03-02 16:05:46 +0200

    Forecast html & css

--- TradingForecasts/TradingForecasts/src/main/java/forexbet/tradingforecasts/web/ForecastController.java
+++ TradingForecasts/TradingForecasts/src/main/java/forexbet/tradingforecasts/web/ForecastController.java
@@ -1,7 +1,40 @@
 package forexbet.tradingforecasts.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/forecasts")
 public class ForecastController {
+
+    @GetMapping("/eur-usd-forecast")
+    public String forecastEurUsd() {
+        return "eur-usd-forecast";
+    }
+
+    @GetMapping("/eur-gbp-forecast")
+    public String forecastEurGbp() {
+        return "eur-gbp-forecast";
+    }
+
+    @GetMapping("/gold-forecast")
+    public String forecastGold() {
+        return "gold-forecast";
+    }
+
+    @GetMapping("/dax-forecast")
+    public String forecastDax() {
+        return "dax-forecast";
+    }
+
+    @GetMapping("/dow-jones-forecast")
+    public String forecastDowJones() {
+        return "dow-jones-forecast";
+    }
+
+    @GetMapping("/nasdaq-forecast")
+    public String forecastNasdaq() {
+        return "nasdaq-forecast";
+    }
 }
