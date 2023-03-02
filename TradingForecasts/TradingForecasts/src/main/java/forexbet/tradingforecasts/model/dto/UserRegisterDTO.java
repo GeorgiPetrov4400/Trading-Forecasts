500565467b4650f0425949d443f6c154e206964f
Georgi Petrov <gap4400@gmail.com>
2023-03-02 12:02:48 +0200

    UserController register and login

---
+++ TradingForecasts/TradingForecasts/src/main/java/forexbet/tradingforecasts/model/dto/UserRegisterDTO.java
@@ -0,0 +1,90 @@
+package forexbet.tradingforecasts.model.dto;
+
+import forexbet.tradingforecasts.model.entity.UserRole;
+import forexbet.tradingforecasts.model.entity.enums.UserRoleEnum;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UserRegisterDTO {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(min = 5, max = 20)
+    private String password;
+    @NotBlank
+    @Size(min = 5, max = 20)
+    private String confirmPassword;
+
+    @NotBlank
+    @Size(min = 2, max = 20)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 2, max = 20)
+    private String lastName;
+
+    public UserRegisterDTO() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserRegisterDTO setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public UserRegisterDTO setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserRegisterDTO setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public UserRegisterDTO setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public UserRegisterDTO setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public UserRegisterDTO setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+}
---
+++ TradingForecasts/TradingForecasts/src/main/java/forexbet/tradingforecasts/model/service/UserServiceModel.java
@@ -0,0 +1,87 @@
+package forexbet.tradingforecasts.model.service;
+
+import forexbet.tradingforecasts.model.entity.UserRole;
+import forexbet.tradingforecasts.model.entity.enums.UserRoleEnum;
+
+public class UserServiceModel {
+
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    private UserRole userRole;
+
+    public UserServiceModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public UserServiceModel setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public UserServiceModel setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserServiceModel setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserServiceModel setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public UserServiceModel setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public UserServiceModel setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public UserServiceModel setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+        return this;
+    }
+}
