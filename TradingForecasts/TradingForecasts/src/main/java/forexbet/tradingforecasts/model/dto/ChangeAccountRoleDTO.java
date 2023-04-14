bc0a944c5aac63c4272e006a900482d0b27dee58
Georgi Petrov <gap4400@gmail.com>
2023-04-14 15:51:40 +0300

    Change roles message

--- TradingForecasts/TradingForecasts/src/main/java/forexbet/tradingforecasts/model/dto/ChangeAccountRoleDTO.java
+++ TradingForecasts/TradingForecasts/src/main/java/forexbet/tradingforecasts/model/dto/ChangeAccountRoleDTO.java
@@ -1,10 +1,25 @@
 package forexbet.tradingforecasts.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class ChangeAccountRoleDTO {
 
+    private String currentRole;
+
+    @NotNull(message = "You have to select role!")
     private String newRole;
 
     public ChangeAccountRoleDTO() {
+    }
+
+    public String getCurrentRole() {
+        return currentRole;
+    }
+
+    public ChangeAccountRoleDTO setCurrentRole(String currentRole) {
+        this.currentRole = currentRole;
+        return this;
     }
 
     public String getNewRole() {
