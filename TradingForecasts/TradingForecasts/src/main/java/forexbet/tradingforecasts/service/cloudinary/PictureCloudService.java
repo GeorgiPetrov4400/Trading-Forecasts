051406ef63022996b01e8b4e9284e839f32b4c97
Georgi Petrov <gap4400@gmail.com>
2023-04-25 16:53:11 +0300

    Refactor Cloudinary

--- TradingForecasts/TradingForecasts/src/main/java/forexbet/tradingforecasts/service/cloudinary/PictureCloudService.java
+++ TradingForecasts/TradingForecasts/src/main/java/forexbet/tradingforecasts/service/cloudinary/PictureCloudService.java
@@ -2,6 +2,7 @@ package forexbet.tradingforecasts.service.cloudinary;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import forexbet.tradingforecasts.config.CloudinaryConfig;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,11 +17,11 @@ public class PictureCloudService {
 
     private final Cloudinary cloudinary;
 
-    public PictureCloudService() {
+    public PictureCloudService(CloudinaryConfig cloudinaryConfig) {
         this.cloudinary = new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "dtg97g3ym",
-                "api_key", "915884395922571",
-                "api_secret", "zuylkA_yRtwWCdKCx46-YygXByU",
+                "cloud_name", cloudinaryConfig.getCloudName(),
+                "api_key", cloudinaryConfig.getApiKey(),
+                "api_secret", cloudinaryConfig.getApiSecret(),
                 "secure", true));
     }
 
