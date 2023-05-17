31427802b63468d958728d96ec894f96d6cad344
Georgi Petrov <gap4400@gmail.com>
2023-05-17 22:20:50 +0300

    Free forecasts view date created

--- TradingForecasts/TradingForecasts/src/main/resources/static/js/free-forecasts.js
+++ TradingForecasts/TradingForecasts/src/main/resources/static/js/free-forecasts.js
@@ -31,7 +31,6 @@ function reloadForecasts() {
             row.appendChild(descriptionCol)
 
             forecastTable.appendChild(row)
-
         }))
 
     forecastContainer.appendChild(forecastTable)
