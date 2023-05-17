3f8053a4da74e94e61206c7bfdce02acdf619fce
Georgi Petrov <gap4400@gmail.com>
2023-05-17 22:17:16 +0300

    Free forecasts view date created

--- TradingForecasts/TradingForecasts/src/main/resources/static/js/free-forecasts.js
+++ TradingForecasts/TradingForecasts/src/main/resources/static/js/free-forecasts.js
@@ -12,6 +12,7 @@ function reloadForecasts() {
 
             let categoryCol = document.createElement('td')
             let typeCol = document.createElement('td')
+            let dateCreated = document.createElement('td')
             let pictureCol = document.createElement('td')
             let img = document.createElement("img")
             img.setAttribute("src", forecast.pictureUrl)
@@ -19,11 +20,13 @@ function reloadForecasts() {
 
             categoryCol.textContent = forecast.category.category
             typeCol.textContent = forecast.forecastType
+            dateCreated.textContent = forecast.created
             pictureCol.appendChild(img)
             descriptionCol.textContent = forecast.description
 
             row.appendChild(categoryCol)
             row.appendChild(typeCol)
+            row.appendChild(dateCreated)
             row.appendChild(pictureCol)
             row.appendChild(descriptionCol)
 
